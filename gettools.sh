a7b505e0b8b148f1711fa8b32b5792bb758397e0
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2020-09-19 15:45:08 +0430

    Update gettools.sh

--- gettools.sh
+++ gettools.sh
@@ -14,7 +14,7 @@ git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarc
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 --depth=1 stock_32
 
 # Clone AnyKernel3
-git clone https://github.com/mashkouk/AnyKernel3.git
+git clone https://github.com/ragy365/AnyKernel3.git
 
 #Download Clang
 if [ ! -d clang ]; then
